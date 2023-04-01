1197a143cc315286852f8c38f19480e240ff1d94
Britt Li Kendle <90359619+Bkendle1@users.noreply.github.com>
2023-04-01 19:40:48 -0400

    Fade out complete
    Object shatters and fades out overtime and destroys itself after it fades out

--- Assets/Scripts/FadeOut.cs
+++ Assets/Scripts/FadeOut.cs
@@ -4,35 +4,24 @@ using UnityEngine;
 
 public class FadeOut : MonoBehaviour
 {
-    private bool fadeOut;
     [SerializeField] private float _fadeSpeed = 2f;
     void Update()
     {
-        if (fadeOut)
+        //access object's color
+        Color objectColor = GetComponent<Renderer>().material.color;
+        Debug.Log("Object's material: " + objectColor);
+        //lower the alpha value of the object's color
+        float fadeAmount = objectColor.a - (_fadeSpeed * Time.deltaTime);
+            
+        //recreate the object's color with the new lowered alpha value
+        objectColor = new Color(objectColor.r, objectColor.b, objectColor.g, fadeAmount);
+        //assign color with new alpha value to object
+        GetComponent<Renderer>().material.color = objectColor;
+            
+        //stop loop once object is transparent because this repeats each frame
+        if (objectColor.a <= 0)
         {
-            //access object's color
-            Color objectColor = GetComponent<Renderer>().material.color;
-            Debug.Log(objectColor);
-            //lower the alpha value of the object's color
-            float fadeAmount = objectColor.a - (_fadeSpeed * Time.deltaTime);
-            
-            //recreate the object's color with the new lowered alpha value
-            objectColor = new Color(objectColor.r, objectColor.b, objectColor.g, fadeAmount);
-            //assign color with new alpha value to object
-            GetComponent<Renderer>().material.color = objectColor;
-            
-            //stop loop once object is transparent because this repeats each frame
-            if (objectColor.a <= 0)
-            {
-                fadeOut = false;
-            }
+            Destroy(transform.parent.gameObject);
         }
-        
-        
-    }
-
-    private void FadeOutObject()
-    {
-        fadeOut = true;
     }
 }
